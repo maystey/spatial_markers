19289c645636ab2cd1c1bab89914184cec8100f9
Mayhew Steyn <mayhew@steyn.co.za>
2019-12-23 11:17:48 +0200

    refactor

--- spatial_markers/spatial_marker_base.gd
+++ spatial_markers/spatial_marker_base.gd
@@ -2,20 +2,19 @@ extends Node2D
 class_name SpatialMarkerBase
 
 ##Variables
-#var parent:Spatial;
 export(float) var radius:float setget set_radius, get_radius; #Make a visual widdget for this
 
 export(Vector3) var spatial_position:Vector3 setget set_spatial_position, get_spatial_position;
 
-func _ready():
+func _ready() -> void:
 	pass
 
 ##Functions
-func set_position_data(is_offscreen:bool, uv:Vector2, displacement:Vector3)->void: 
+func set_position_data(is_offscreen:bool, uv:Vector2, displacement:Vector3) -> void: 
 	pass
 
 ##Setters and getters
-func set_spatial_position(value:Vector3)-> void:
+func set_spatial_position(value:Vector3) -> void:
 	spatial_position = value;
 	
 func get_spatial_position() -> Vector3:
@@ -24,5 +23,5 @@ func get_spatial_position() -> Vector3:
 func set_radius(value) -> void:
 	radius = value;
 
-func get_radius():
+func get_radius() -> float:
 	return radius;
--- spatial_markers/spatial_marker_manager.gd
+++ spatial_markers/spatial_marker_manager.gd
@@ -4,32 +4,27 @@ class_name SpatialMarkerManager
 var camera:Camera;
 
 ##Variables
-#y_axis items behind shown to left/right
-#x_axis same as above but up/down (unlikely to be used but easy to implement)
-#back_bottom items behidn mapped to bottom of screen)
-#pole directionally invariant
-enum BehindCameraUnwrap {down, y_axis, x_axis, pole};
+#down - items behind the camera mapped to bottom of screen)
+#left_right - items behind the camera shown to left/right
+#up_down - same as above but up/down (unlikely to be used but easy to implement)
+#pole directionally invariant (coming soon)
+enum BehindCameraMap{down, left_right, up_down, pole};
 
-export(BehindCameraUnwrap) var behind_camera_unwrap = BehindCameraUnwrap.y_axis;
+export(BehindCameraMap) var behind_camera_map = BehindCameraMap.left_right;
 
-var spatial_icons := []; #make this a map that uses the owner?
+var spatial_icons := [];
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+## Functions
+### Overrides
+func _ready() -> void:
 	camera = get_viewport().get_camera() as Camera;
-	print(get_viewport_rect().size);
-	pass # Replace with function body.
+#	print(get_viewport_rect().size);
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	for si in spatial_icons:
 		var position := si.get_spatial_position() as Vector3;
 #
 		var screen_pos := camera.unproject_position(position) as Vector2;
-		
-#		var uv := _position_to_uv(si.get_spatial_position()) as Vector2;
-		
-#		var margins := (Vector2(1,1) - Vector2(1,1)*si.radius/get_viewport_rect().size) as Vector2;
 		
 		var margins := (get_viewport_rect().size - Vector2(1,1)*si.radius) as Vector2;
 		
@@ -40,38 +35,31 @@ func _process(delta):
 		##Check if position is behind camera and adjust the uv
 		if camera.is_position_behind(position):
 			is_offscreen = true;
-#			uv = Vector2(1,1) - uv;
 			screen_pos = get_viewport_rect().size - screen_pos;
 			
-			if behind_camera_unwrap == BehindCameraUnwrap.down:
+			if behind_camera_map == BehindCameraMap.down:
 				screen_pos.y = get_viewport_rect().size.y;
-			if behind_camera_unwrap == BehindCameraUnwrap.y_axis:
-				screen_pos.x = _axis_unwrap(screen_pos, 0);
-			elif behind_camera_unwrap == BehindCameraUnwrap.x_axis:
-				screen_pos.y = _axis_unwrap(screen_pos, 1);
+			if behind_camera_map == BehindCameraMap.left_right:
+				screen_pos.x = _axis_map(screen_pos, 0);
+			elif behind_camera_map == BehindCameraMap.up_down:
+				screen_pos.y = _axis_map(screen_pos, 1);
 		
 		##Confine the uv to the bounds of the screen
 		screen_pos.x = clamp(screen_pos.x, si.radius, margins.x);
 		screen_pos.y = clamp(screen_pos.y, si.radius, margins.y);
-#		uv.x = clamp(uv.x, si.radius, get_viewport_rect().size.x - si.radius); #include radius, need to actually do if statement for arrow
-#		uv.y = clamp(uv.y, si.radius , get_viewport_rect().size.y - si.radius); #ditto
-		
+
 		si.set_global_position(screen_pos);
 		si.set_position_data(is_offscreen, screen_pos/get_viewport_rect().size, 
 								camera.get_global_transform().origin - position);
 	pass
 
-##Functions
-
-#func _screen_to_uv(position:Vector2):
-#	return position/get_viewport_rect().size;
-
-func _axis_unwrap(position:Vector2, axis:int)->float:
+### Map Functions
+func _axis_map(position:Vector2, axis:int)->float:
 	if position[axis] < 0.5*get_viewport_rect().size[axis]:
 		return position[axis] - 0.5*get_viewport_rect().size[axis]
 	return position[axis] + 0.5*get_viewport_rect().size[axis]
 
-func _pole_unwrap(position:Vector2) -> Vector2:
+func _pole_map(position:Vector2) -> Vector2:
 	if (position.x < 0 or position.x > get_viewport_rect().size.x) or\
 		(position.y < 0 or position.y > get_viewport_rect().size.y):
 			#Outside camera area
@@ -86,11 +74,7 @@ func _pole_unwrap(position:Vector2) -> Vector2:
 func _back_map_down(position:Vector2) -> Vector2:
 	return Vector2(position.x, get_viewport_rect().size.y)
 
-func _axis_unwrap_uv(uv:Vector2, axis:int):
-	if uv[axis] < 0.5:
-		return uv[axis] - 0.5;
-	return uv[axis] + 0.5;
-
+### Misc Functions
 func add_spatial_icon(spatial_icon):
 	spatial_icons.append(spatial_icon);
 	pass
